67a173ca1533df77c2d6a67abe8b27f64224f742
loxishviligela1-hub <loxishviligela1@gmail.com>
2025-09-13 17:19:19 +0400

    Add files via upload

---
+++ main2.js.js
@@ -0,0 +1,55 @@
+// function ctof(c) {
+//   return (c*9/5)+32
+// }
+
+// console.log(ctof(0))       
+
+
+// function reversed(str) {
+//   return str.split("").reverse().join("")
+// }
+
+// console.log(reversed("hello"))
+
+// აქ მხოლოდ reverse() დავწერე და  split და join ი დამიწერა ავტომატურად
+
+// function countWords(sentence) {
+//   let words = sentence.trim().split(" ")
+//   return words.length
+// }
+ 
+// console.log(countWords("Hello world"))
+
+
+// function countvowels(word) {
+//     let vowels = "aeiou"
+//     let count = 0}
+//     return count{
+// }
+//     console.log(countVowels("hello"))
+
+//     ვერ გავიგე მემგონი if სჭირდება
+
+
+// function sumnumbers(num) {
+//   let sum = 0
+//   for (let i = 0 i <= num; i++) {
+//     if (i % 2 === 0) {
+//       sum += i
+//     }
+
+// console.log(sumnumbers(10))
+
+
+// function correctPassword(password) {
+//   let hasnumber = 0-9.test(password)
+//   let hasuppercase = A-Z.test(password)
+
+//   if (password.length > 8 && hasnumber && hasuppercase) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+
