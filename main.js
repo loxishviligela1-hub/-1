943382cef79d54739f8b686a81b1076701bc75f6
loxishviligela1-hub <loxishviligela1@gmail.com>
2025-09-13 17:16:32 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,44 @@
-// function ctof(c) {
-//   return (c*9/5)+32
+1// function getAbbr(name) {
+//   if  return "";
+//   const parts = name.trim().split(/\s+/);
+//   const initials = parts.map(function(p) {
+//     return p.charAt(0).toUpperCase()
+//   });
+//   return initials.join(".");
 // }
 
-// console.log(ctof(0))       
+//  console.log(getAbbr("John Doe"))
 
 
-// function reversed(str) {
-//   return str.split("").reverse().join("")
-// }
 
-// console.log(reversed("hello"))
-
-// აქ მხოლოდ reverse() დავწერე და  split და join ი დამიწერა ავტომატურად
-
-// function countWords(sentence) {
-//   let words = sentence.trim().split(" ")
-//   return words.length
-// }
- 
-// console.log(countWords("Hello world"))
-
-
-// function countvowels(word) {
-//     let vowels = "aeiou"
-//     let count = 0}
-//     return count{
-// }
-//     console.log(countVowels("hello"))
-
-//     ვერ გავიგე მემგონი if სჭირდება
-
-
-// function sumnumbers(num) {
-//   let sum = 0
-//   for (let i = 0 i <= num; i++) {
-//     if (i % 2 === 0) {
-//       sum += i
+// 4function removeSpaces(text) {
+//   let result = ""
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i] !== " ") {  
+//       result = result + text[i]
 //     }
-
-// console.log(sumnumbers(10))
-
-
-// function correctPassword(password) {
-//   let hasnumber = 0-9.test(password)
-//   let hasuppercase = A-Z.test(password)
-
-//   if (password.length > 8 && hasnumber && hasuppercase) {
-//     return true
-//   } else {
-//     return false
 //   }
+
+//   return result;
 // }
 
+// console.log(removeSpaces("L A M P A R I "));
+
+
+
+function reverseeachword(sentence) {
+  const words = sentence.split(' ')
+  const reversedWords = []
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i]
+    let reversed = ''
+    for (let j = word.length - 1; j >= 0; j--) {
+      reversed += word[j]
+    }
+    reversedWords.push(reversed)
+  }
+  return reversedWords.join(' ')
+}
+
+console.log(reverseEachWord('ნაპოლეონ ბონაპარტი'))
 
